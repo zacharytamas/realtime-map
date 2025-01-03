3620a49ebb49dfd489fd5cbd61a0dc0977222f13
Zachary Jones <zachary@jones.cx>
2025-01-03 14:10:23 -0500

    Add Mapbox and begin playing around

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useRef } from 'react'
+import mapboxgl from 'mapbox-gl'
 import './App.css'
+import mapboxConfig from './mapbox-config'
+
+mapboxgl.accessToken = mapboxConfig.accessToken
 
 function App() {
-  return (
-    <>
-      <div>Hello</div>
-    </>
-  )
+  const mapContainer = useRef<HTMLDivElement>(null)
+  const map = useRef<mapboxgl.Map | null>(null)
+
+  useEffect(() => {
+    if (!mapContainer.current) return
+
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/dark-v11',
+      center: [-122.4194, 37.7749], // San Francisco coordinates
+      zoom: 12,
+      pitch: 75,
+    })
+
+    return () => {
+      map.current?.remove()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!map.current) return
+
+    new mapboxgl.Marker().setLngLat([-122.4194, 37.7749]).addTo(map.current)
+  }, [map.current])
+
+  return <div className="map-container" ref={mapContainer} />
 }
 
 export default App
